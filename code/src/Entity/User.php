c792e1ba02b9c4449280a2fdebb1be10e2369b3d
danielp67 <daniel.67100@hotmail.fr>
2020-11-20 16:27:33 +0100

    testunit

--- code/src/Entity/User.php
+++ code/src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Exception;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -11,6 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User
 {
+    private const PATTERN_MAIL = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/';
+    private const PATTERN_PASS = '/^[a-zA-Z0-9_]{6,12}$/';
+    private const PATTERN_LOGIN = '/^[a-zA-Z0-9À-ÿ_.-]{2,16}$/';
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -31,11 +36,27 @@ class User
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $email;
+    private $mail;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     * @param int $id
+     * @throws Exception
+     * @return  self
+     */
+    public function setId(int $id): self
+    {
+        if (! is_int($id) || $id < 0) {
+            throw new Exception('Id invalide');
+        }
+        $this->id = $id;
+
+        return $this;
     }
 
     public function getLogin(): ?string
@@ -45,6 +66,10 @@ class User
 
     public function setLogin(string $login): self
     {
+        $pattern = self::PATTERN_LOGIN;
+        if (! preg_match($pattern, $login)) {
+            throw new Exception('login est invalide');
+        }
         $this->login = $login;
 
         return $this;
@@ -57,19 +82,27 @@ class User
 
     public function setPassword(string $password): self
     {
+        $pattern = self::PATTERN_PASS;
+        if (! preg_match($pattern, $password)) {
+            throw new Exception('password est invalide');
+        }
         $this->password = $password;
 
         return $this;
     }
 
-    public function getEmail(): ?string
+    public function getMail(): ?string
     {
-        return $this->email;
+        return $this->mail;
     }
 
-    public function setEmail(string $email): self
+    public function setMail(string $mail): self
     {
-        $this->email = $email;
+        $pattern = self::PATTERN_MAIL;
+        if (! preg_match($pattern, $mail)) {
+            throw new Exception('mail est invalide');
+        }
+        $this->mail = $mail;
 
         return $this;
     }
---
+++ code/tests/UserTest.php
@@ -0,0 +1,117 @@
+<?php
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+
+    private const PATTERN_MAIL = '/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/';
+    private const PATTERN_LOGIN = '/^[a-zA-Z0-9À-ÿ_.-]{2,16}$/';
+    private const PATTERN_PASS = '/^[a-zA-Z0-9_]{6,12}$/';
+    private $user;
+
+    public function index()
+    {
+        $this->user = new User();
+    }
+
+    public function testAssertInstanceOfUser()
+    {
+        $this->index();
+        $this->assertInstanceOf(User::class, $this->user);
+        $this->assertClassHasAttribute('login', User::class);
+        $this->assertClassHasAttribute('password', User::class);
+        $this->assertClassHasAttribute('mail', User::class);
+    }
+
+     /**
+     * @dataProvider additionProviderId
+     */
+    public function testSetId($userId)
+    {
+        $this->index();
+        $this->user->setId($userId);
+        $this->assertIsInt($this->user->getId($userId));
+    }
+
+    public function additionProviderId()
+    {
+        return [
+          [1],
+          [23],
+          [258],
+          [45.5]
+        ];
+    }
+
+    /**
+     * @dataProvider additionProviderMail
+     */
+   public function testSetMail($mail)
+    {
+        $pattern = self::PATTERN_MAIL;
+
+        $this->index();
+        $this->user->setMail($mail);
+        $this->assertMatchesRegularExpression($pattern, $this->user->getMail($mail));
+    }
+
+    public function additionProviderMail()
+    {
+        return [
+            ['test@test.com'],
+            ['lalalla@d.fr'],
+            ['g@te.de'],
+            ['gmail-dudu@tetest.fdsfd.de']
+        ];
+    }
+
+    /**
+     * @dataProvider additionProviderLogin
+     */
+    public function testSetLogin($login)
+    {
+        $pattern = self::PATTERN_LOGIN;
+
+        $this->index();
+        $this->user->setLogin($login);
+        $this->assertMatchesRegularExpression($pattern, $this->user->getLogin($login));
+    }
+
+
+    public function additionProviderLogin()
+    {
+        return [
+            ['fgrej-fdssd'],
+            ['élaHfjfiod'],
+            ['ÀteÀde_86'],
+            ['fdsff.dfzadde'],
+            ['Username']
+        ];
+    }
+
+    /**
+     * @dataProvider additionProviderPassword
+     */
+    public function testSetPassword($pass)
+    {
+        $pattern = self::PATTERN_PASS;
+        $this->index();
+        $this->user->setPassword($pass);
+        $this->assertMatchesRegularExpression($pattern, $this->user->getPassword($pass));
+    }
+
+    
+
+    public function additionProviderPassword()
+    {
+        return [
+            ['fsd456'],
+            ['HlHfjfiod'],
+            ['ateade_86'],
+            ['12lettresUni']
+        ];
+    }
+
+}
