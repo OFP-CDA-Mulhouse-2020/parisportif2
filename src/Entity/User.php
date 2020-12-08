5dc0fe138610abc21b48a98874cecc9707efd6de
danielp67 <daniel.67100@hotmail.fr>
2020-12-08 14:18:06 +0100

    correction on User with unique Email OK

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,7 +12,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"},
+ * groups={"register"},
+ * message="Il y a déjà un compte avec cet email")
  */
 class User implements UserInterface
 {
