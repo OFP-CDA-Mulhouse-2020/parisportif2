2ad8dbdfcb1e06867b6eee5912e08aa0d9d0a341
LadG102 <l.gakou1@gmail.com>
2021-01-07 15:44:33 +0100

    tdd functional tests in HomeControllerTest are done

--- tests/Functional/HomeControllerTest.php
+++ tests/Functional/HomeControllerTest.php
@@ -1,39 +1,37 @@
 <?php
 
 // login: ladji.cda@test.com
-//// mdp : M1cdacda8
+// mdp : M1cdacda8
 namespace App\Tests\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
 
-
     public function testIfPageExist(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $form = $crawler->filter('form')->form();
-        $form['email'] = 'ladji.cda@test.com';
-        $form['password'] = 'M1cdacda8';
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $client->request('GET', '/app');
 
-
-        $this->assertResponseRedirects('/app');
+        $this->assertResponseStatusCodeSame(200);
     }
 
     // HEADER
     public function testHeaderStructure(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $form = $crawler->filter('form')->form();
-        $form['email'] = 'ladji.cda@test.com';
-        $form['password'] = 'M1cdacda8';
-
-        $crawler = $client->followRedirect();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
 
         $this->assertSelectorExists('header');
+        $this->assertEquals(2, $crawler->filter('header li.dropdown')->count());
 
         // LOGO
         $this->assertSelectorExists('#logo');
@@ -48,34 +46,160 @@ class HomeControllerTest extends WebTestCase
         $this->assertEquals(10, $crawler->filter('nav.nav-secondary li.nav-item>a[class~="nav-link"]')->count());
     }
 
+    public function testHeaderNavDropdownContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
 
-    // SECTION#CONTENT
+        // dropdown 1
+        $this->assertEquals(5, $crawler->filter('.dropdown-1 a.dropdown-item')->count());
+
+        // dropdown 2
+        $this->assertEquals(4, $crawler->filter('.dropdown-2 a.dropdown-item')->count());
+    }
+
+
+    // SECTION#PAGE-CONTENT
     public function testContentSectionStructure(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/login');
-        $form = $crawler->filter('form')->form();
-        $form['email'] = 'ladji.cda@test.com';
-        $form['password'] = 'M1cdacda8';
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
 
-        $crawler = $client->followRedirect();
-
-        $this->assertSelectorExists('section#content');
+        $this->assertSelectorExists('section#page-content');
 
         // ASIDE LEFT
         $this->assertSelectorExists('aside.aside-left');
-        // Encart promotionnel
+
+        // SECTION CENTRALE
+        $this->assertSelectorExists('section.central');
+
+        // ASIDE RIGHT
+        $this->assertSelectorExists('aside.aside-right');
+    }
+
+
+    //ASIDE LEFT CONTENT
+    public function testAsideLeftContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        // Advertising -Insert-1
         $this->assertEquals(1, $crawler->filter('aside.aside-left div.advertising-insert-1')->count());
         // Navigation Pari
         $this->assertEquals(1, $crawler->filter('aside.aside-left div.nav-bet')->count());
+    }
 
-        // SECTION CENTRAL
-        $this->assertSelectorExists('section.central');
+    public function testNavBetContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
 
-        $this->assertEquals(1, $crawler->filter('section.central div.carousel')->count());
+        $this->assertSelectorExists('div.nav-bet>div.accordion');
+        $this->assertEquals(4, $crawler->filter('div.nav-bet button.accordion-button')->count());
+    }
+
+
+    //CENTRAL SECTION CONTENT
+    public function testSectionCentralContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        // Carousel
         $this->assertEquals(1, $crawler->filter('div#carouselExampleIndicators')->count());
 
-        //Tableau prise de paris
+        //Bet Board
         $this->assertEquals(1, $crawler->filter('section.central div.bet-board')->count());
+    }
+
+    public function testCarouselContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        // Carousel
+        $this->assertGreaterThanOrEqual(1, $crawler->filter('img')->count());
+        $this->assertEquals(1, $crawler->filter('a[class="carousel-control-prev"]')->count());
+        $this->assertEquals(1, $crawler->filter('a[class="carousel-control-next"]')->count());
+    }
+
+
+    //ASIDE RIGHT CONTENT
+    public function testAsideRightContent(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        $this->assertEquals(3, $crawler->filter('aside.aside-right section')->count());
+
+
+        $this->assertSelectorExists('section.bet-ticket');
+        $this->assertSelectorExists('section.bet-search');
+        $this->assertSelectorExists('section.advertising-insert-2');
+    }
+
+    //Ticket section
+    public function testBetTicketSection(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        $this->assertSelectorTextContains('', 'Ticket de paris');
+        $this->assertSelectorTextContains('', 'Ajouter des paris');
+        $this->assertSelectorTextContains('.bet-ticket button', 'Pariez !');
+    }
+
+    //Search section
+    public function testBetSearch(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        $this->assertSelectorTextContains('', 'Chercher un pari');
+        $this->assertSelectorExists('section.bet-search input[type="text"]');
+        $this->assertSelectorExists('section.bet-search button[type="submit"]');
+    }
+
+    //FOOTER
+    public function testFooterStructure(): void
+    {
+        $client = static::createClient();
+        $userRepository = static::$container->get(UserRepository::class);
+        $testUser = $userRepository->findOneByEmail('ladji.cda@test.com');
+        $client->loginUser($testUser);
+        $crawler = $client->request('GET', '/app');
+
+        $this->assertSelectorExists('footer');
+        $this->assertSelectorExists('.footer-content');
+        $this->assertSelectorExists('.footer-disclaimer');
+        $this->assertSelectorExists('.footer-copyright');
     }
 }
