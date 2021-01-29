436ca63646a0f2e8b88d336a2c63d28e060a3f57
mkayz68 <m.kaizerli@gmail.com>
2021-01-29 12:01:19 +0100

    mise en place des paris par sport 12H

--- src/Controller/SportsController.php
+++ src/Controller/SportsController.php
@@ -13,21 +13,14 @@ class SportsController extends AbstractController
 {
     /**
      * @Route("/app/sports/{sports}", name="app_sports")
-     * @param $sports
-     * @param BetRepository $betRepository
-     * @return Response
      */
-    public function index(int $sports, BetRepository $betRepository): Response
+    public function index(string $sports, BetRepository $betRepository): Response
     {
-        $sports = new Sport();
-        //dd($sports);
-        //$listOfBet = $betRepository->findby(['betOpened' => true]);
-        //$listOfBet = $betRepository->find([2 => true]);
-        $listOfBet = $betRepository->find($sports = true);
-        dd($listOfBet);
-        //dd($sports);
+        $listOfBet = $betRepository->findBetBySport($sports);
+        //dd($findBetBySport);
         return $this->render('sports/index.html.twig', [
             'controller_name' => 'SportsController',
+            'listOfBet' => $listOfBet
         ]);
     }
 }
