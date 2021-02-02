15a2147674456e812033d284be5c8a86d6b35aa5
mkayz68 <m.kaizerli@gmail.com>
2021-02-02 11:11:18 +0100

    gestion de l'affichage des sports par page finie

--- src/Controller/SportsController.php
+++ src/Controller/SportsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Sport;
 use App\Repository\BetRepository;
+use App\Repository\CompetitionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,14 +14,24 @@ class SportsController extends AbstractController
 {
     /**
      * @Route("/app/sports/{sports}", name="app_sports")
+     * @param string $sports
+     * @param BetRepository $betRepository
+     * @param CompetitionRepository $competitionRepository
+     * @return Response
      */
-    public function index(string $sports, BetRepository $betRepository): Response
-    {
+    public function index(
+        string $sports,
+        BetRepository $betRepository,
+        CompetitionRepository $competitionRepository
+    ): Response {
         $listOfBet = $betRepository->findBetBySport($sports);
+        $listOfCompetition = $competitionRepository->findCompetitionBySport($sports);
         //dd($findBetBySport);
+        //dd($listOfCompetition);
         return $this->render('sports/index.html.twig', [
             'controller_name' => 'SportsController',
-            'listOfBet' => $listOfBet
+            'listOfBet' => $listOfBet,
+            'listOfCompetition' => $listOfCompetition,
         ]);
     }
 }
