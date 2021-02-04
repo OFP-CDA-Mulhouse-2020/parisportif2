15437a1afcac4c5c9e2be08a865532c4e33b6918
danielp67 <daniel.67100@hotmail.fr>
2021-02-04 11:47:01 +0100

    2021-02-04 #103 New pages and new fixtures added

--- src/Controller/SportsController.php
+++ src/Controller/SportsController.php
@@ -2,36 +2,50 @@
 
 namespace App\Controller;
 
-use App\Entity\Sport;
+use App\Entity\Cart;
+use App\Entity\User;
 use App\Repository\BetRepository;
 use App\Repository\CompetitionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Bet;
 
 class SportsController extends AbstractController
 {
     /**
-     * @Route("/app/sports/{sports}", name="app_sports")
-     * @param string $sports
+     * @Route("/app/sports/{typeOfSport}", name="app_sports")
+     * @param string $typeOfSport
      * @param BetRepository $betRepository
      * @param CompetitionRepository $competitionRepository
      * @return Response
      */
     public function index(
-        string $sports,
+        string $typeOfSport,
         BetRepository $betRepository,
         CompetitionRepository $competitionRepository
     ): Response {
-        $listOfBet = $betRepository->findBetBySport($sports);
-        $listOfCompetition = $competitionRepository->findCompetitionBySport($sports);
-        //dd($findBetBySport);
-        //dd($listOfCompetition);
-        return $this->render('sports/index.html.twig', [
+
+        $listOfBet = $betRepository->findBetBySport($typeOfSport);
+        $listOfCompetition = $competitionRepository->findCompetitionBySport($typeOfSport);
+
+        $user = $this->getUser();
+        assert($user instanceof User);
+
+        /** @var Cart|null $cart */
+        $cart = $user->getCart();
+
+        if ($cart) {
+            $items = $cart->getItems();
+        } else {
+            $items = null;
+        }
+
+        return $this->render('sports/sports.html.twig', [
             'controller_name' => 'SportsController',
             'listOfBet' => $listOfBet,
             'listOfCompetition' => $listOfCompetition,
+            'cart' => $cart,
+            'items' => $items,
         ]);
     }
 }
