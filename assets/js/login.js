e8ea003426a1699a6be07c4dd620b06edebb0ba3
mkayz68 <m.kaizerli@gmail.com>
2021-02-19 16:16:25 +0100

    pages dynamiques en JS suite

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,44 @@
-
 export default function login()
 {
     console.log('test login');
-}
+    /* 1er Form */
+    const editForm = document.querySelector('.edit_email');
+    const editBtnEmail = document.querySelector('.button_edit_email');
+    console.log(editForm);
+    console.log(editBtnEmail);
+    /* 2eme Form */
+    const editEmailDisable = document.querySelector('.edit_email_disable');
+    const btnEditEmailDisable = document.querySelector('.button_edit_email_disable');
+    console.log(editEmailDisable);
+    console.log(btnEditEmailDisable);
+    /* 3eme FORM */
+    const passwordForm = document.querySelector('.edit_password');
+    const btn_form_password = document.querySelector('.btn_form_password');
+    console.log(passwordForm);
+    console.log(btn_form_password);
+    /* 4eme FORM */
+    const edit_password_disable = document.querySelector('.edit_password_disable');
+    const btn_edit_password_disable = document.querySelector('.btn_edit_password_disable');
+    console.log(edit_password_disable);
+    console.log(btn_edit_password_disable);
+    /* function active */
 
+    /* 1er Form */
+    editForm.classList.add('d-none');
+    editBtnEmail.addEventListener('click', (e) => {
+            console.log('click')
+            editForm.classList.add("d-none");
+            editEmailDisable.classList.remove('d-none');
+        })
+    /* 2eme Form */
+    btnEditEmailDisable.addEventListener('click', (e) => {
+        console.log('click')
+        editEmailDisable.classList.add('d-none');
+        editForm.classList.remove('d-none')
+    })
+
+    /* 3eme Form */
+    btn_form_password.addEventListener('click', (e) => {
+        console.log('click')
+    })
+}
